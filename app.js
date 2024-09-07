ce0dc6094276e9bcf70854097903962dd685454b
Caique-Rosa <cayo.h.rosa@gmail.com>
2024-09-07 16:57:41 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+
+//let section = document.getElementById("resultados-pesquisa")
+//console.log(section);
+
+//let resultados = ""
+
+//for (let dado of dados) {
+    //resultados += `
+    //<div class="item-resultado">
+       //<h2>
+            //<href="https://www.gov.br/inpi/pt-br/servicos/patentes/minha-primeira-patente/minha-primeira-patente" target="_blank" rel="noopener noreferrer">${dado.titulo}</a>
+       // </h2>
+        //<p class="descricao-meta">${dado.descricao}</p>
+        //<a href="https://www.gov.br/inpi/pt-br/servicos/patentes/minha-primeira-patente/minha-primeira-patente" target="_blank">Descubra como</a>
+        //<a href="${dado.link}" target="_blank" rel="noopener noreferrer"></a>
+            
+   // </div>
+
+//`
+//}
+
+//section.innerHTML = resultados
+
---
+++ dados.js
@@ -0,0 +1,32 @@
+let dados = [
+    {
+      titulo: "Minha Primeira Patente",
+      descricao: "Quer registrar sua invenção e não sabe por onde começar? O INPI disponibiliza um guia completo e eu estou aqui para te ajudar a tirar o seu projeto do papel. Siga o passo a passo e vamos juntos dar vida à sua ideia!",
+      link: "https://www.gov.br/inpi/pt-br/servicos/patentes/minha-primeira-patente/minha-primeira-patente"
+    },
+    {
+      titulo: "Passo 1: Pesquise a Novidade da Sua Ideia",
+      descricao: "Antes de dar o próximo passo, é fundamental pesquisar se sua ideia já existe. Utilize as ferramentas do INPI para verificar a viabilidade da sua invenção.",
+      link: "hhttps://www.gov.br/inpi/pt-br/servicos/patentes/guia-basico/busca-de-patentes"
+    },
+    {
+      titulo: "Passo 2: Requisito Específico por Tipo de Invento",
+      descricao: "Antes de iniciar um pedido, você precisa entender se o que deseja patentear é uma invenção ou um modelo de utilidade. As invenções estão ligadas a novas tecnologias e podem ser um novo produto ou um novo processo. Já os modelos de utilidade são sempre objetos, que apresentam nova forma ou estrutura e melhoria funcional em relação aos objetos já conhecidos.",
+      link: "https://www.gov.br/inpi/pt-br/servicos/patentes/minha-primeira-patente/Requisitosespecficosportipodeinvenov3.xls/view"
+    },
+    {
+      titulo: "Passo 3: Inicie o seu Pedido de Patente",
+      descricao: "Se depois de fazer as buscas você achar que sua invenção ou seu modelo de utilidade realmente são diferentes do que já existe, então é hora de começar a escrever seu pedido.",
+      link: "https://www.gov.br/inpi/pt-br/servicos/patentes/minha-primeira-patente/faca-o-download-dos-arquivos-separadamente"
+    },
+    {
+      titulo: "Passo 4: Deposite no INPI",
+      descricao: "Para garantir que sua ideia inovadora seja protegida por lei e ninguém mais possa copiá-la, você precisa registrar uma patente no INPI. Depois de preparar toda a documentação necessária, é hora de dar entrada no pedido de patente de forma online.",
+      link: "https://www.gov.br/inpi/pt-br/servicos/patentes/tutorial-de-deposito"
+    },
+    {
+      titulo: "Passo 5: Acompanhe",
+      descricao: "Sua ideia vai fazer uma jornada até virar patente. Agora que você já deu entrada no pedido, os especialistas do INPI darão uma olhada pra ver se tá tudo certo. Em seguida, sua ideia vira notícia pra todo mundo. Depois de uma análise mais detalhada, eles te darão o veredito final. Importante: Não esquece de acompanhar de perto cada passo dessa aventura!",
+      link: "https://www.gov.br/inpi/pt-br/servicos/patentes/guia-basico"
+    }
+  ];
